91765f798936033a5fea278cc45cdf526f5677f3
Ramiro <ramiropascual1@hotmail.com>
2022-06-20 11:05:56 -0300

    cambiando

--- source/index.js
+++ source/index.js
@@ -7,3 +7,7 @@ const public =path.resolve(__dirname, '../public')
 server.use(express.static(public))
 
 server.get('/', (req, res) => res.sendFile(path.resolve(__dirname, 'views/home.html')))
+
+server.get('/login', (req, res) => res.sendFile(path.resolve(__dirname, 'views/login.html')))
+
+server.get('/register', (req, res) => res.sendFile(path.resolve(__dirname, 'views/register.html')))
